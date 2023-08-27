8ff5c1cd4c13b3b051b97e37fcb91d738abe1f29
Hakim Ahmed <hakahmed@c1r17s4.42madrid.com>
2023-08-27 13:27:00 +0200

    Initial commit (file structure created)

---
+++ include/logging.hpp
@@ -0,0 +1,8 @@
+#ifndef __LOGGING__
+# define __LOGGING__
+
+# include <iostream>
+
+# define LOG(x) std::cout << x << std::endl
+
+#endif // __LOGGING__
---
+++ src/main/main.cpp
@@ -0,0 +1,6 @@
+#include "logging.hpp"
+
+int main() {
+    LOG("Hello, ft_curl!");
+    return 0;
+}
